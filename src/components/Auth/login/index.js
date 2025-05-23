a52356a608f494b0cc50035684956ea5853eece8
Retro087 <112253850+Retro087@users.noreply.github.com>
2025-05-23 11:49:31 +0300

    update auth

--- src/components/Auth/login/index.js
+++ src/components/Auth/login/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import s from "./style.module.css";
 import AuthInput from "../../common/input";
-import Input from "../../common/input";
-const Login = (props) => {
+import card from "../../../assets/image.png";
+import google from "../../../assets/google.png";
+import apple from "../../../assets/devicon_apple.png";
+const Registration = (props) => {
   const [data, setData] = useState({ login: "", password: "" });
 
   return (
@@ -15,32 +17,72 @@ const Login = (props) => {
     >
       <div className={s.container}>
         <div className={s.left}>
-          <h2 className={s.left_title}>Войдите, чтобы иметь возможность:</h2>
-          <ul className={s.left_list}>
-            <li className={s.left_item}>Добавлять в избранное</li>
-            <li className={s.left_item}>Писать продавцам</li>
-            <li className={s.left_item}>Размещать товары</li>
-          </ul>
+          <div className={s.left_card}>
+            <div className={s.left_card_text}>
+              <h1>Добро пожаловать в StarLine</h1>
+              <p>С возвращением</p>
+            </div>
+            <img src={card} />
+          </div>
         </div>
         <div className={s.rigth}>
-          <Input
+          <div className={s.logo}>StarLine</div>
+          <h1 className={s.title}>Войти в аккаунт </h1>
+          <AuthInput
             value={data.login}
+            ph="email@gmail.com"
             onChange={(e) => setData({ ...data, login: e.target.value })}
-            label={"Логин"}
+            label={"Электронная почта"}
           />
-          <Input
+          <AuthInput
             value={data.password}
+            ph="Password"
             onChange={(e) => setData({ ...data, password: e.target.value })}
             label={"Пароль"}
           />
           <div className={s.btn_block}>
-            <button type="submit" className={s.btn}>
+            <button
+              style={{ marginBottom: 50 }}
+              type={"submit"}
+              className={s.btn}
+            >
               Войти
             </button>
           </div>
-
-          <div className={s.reg} onClick={props.toReg}>
-            Зарегистрироваться
+          <div className={s.spacer}>
+            <span>Или</span>
+          </div>
+          <div className={s.btn_block}>
+            <button
+              style={{
+                marginBottom: 10,
+                gap: 5,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              type="button"
+              className={s.btn}
+            >
+              <img src={google} />
+              Google
+            </button>
+            <button
+              type="button"
+              className={s.btn}
+              style={{
+                gap: 5,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <img src={apple} />
+              Apple
+            </button>
+          </div>
+          <div className={s.reg} onClick={() => props.toReg()}>
+            Нет аккаута? <span>Зарегистрируйся</span>
           </div>
         </div>
       </div>
@@ -48,4 +90,4 @@ const Login = (props) => {
   );
 };
 
-export default Login;
+export default Registration;
--- src/components/Auth/registration/index.js
+++ src/components/Auth/registration/index.js
@@ -1,8 +1,53 @@
 import React, { useState } from "react";
 import s from "./style.module.css";
 import AuthInput from "../../common/input";
+import card from "../../../assets/image 21.png";
+import google from "../../../assets/google.png";
+import apple from "../../../assets/devicon_apple.png";
 const Registration = (props) => {
   const [data, setData] = useState({ login: "", password: "", name: "" });
+  const [step, setStep] = useState(1);
+
+  const getInputByStep = () => {
+    switch (step) {
+      case 1:
+        return (
+          <AuthInput
+            value={data.login}
+            ph="email@gmail.com"
+            onChange={(e) => setData({ ...data, login: e.target.value })}
+            label={"Электронная почта"}
+          />
+        );
+      case 2:
+        return (
+          <AuthInput
+            value={data.password}
+            ph="password"
+            onChange={(e) => setData({ ...data, password: e.target.value })}
+            label={"Пароль"}
+          />
+        );
+      case 3:
+        return (
+          <AuthInput
+            value={data.name}
+            ph="Name"
+            onChange={(e) => setData({ ...data, name: e.target.value })}
+            label={"Имя"}
+          />
+        );
+      default:
+        return (
+          <AuthInput
+            value={data.login}
+            ph="email@gmail.com"
+            onChange={(e) => setData({ ...data, login: e.target.value })}
+            label={"Электронная почта"}
+          />
+        );
+    }
+  };
 
   return (
     <form
@@ -14,38 +59,65 @@ const Registration = (props) => {
     >
       <div className={s.container}>
         <div className={s.left}>
-          <h2 className={s.left_title}>
-            Зарегистрируйтесь, чтобы иметь возможность:
-          </h2>
-          <ul className={s.left_list}>
-            <li className={s.left_item}>Добавлять в избранное</li>
-            <li className={s.left_item}>Писать продавцам</li>
-            <li className={s.left_item}>Размещать товары</li>
-          </ul>
+          <div className={s.left_card}>
+            <div className={s.left_card_text}>
+              <h1>Добро пожаловать в StarLine</h1>
+              <p>Создайте свой продукт </p>
+            </div>
+            <img src={card} />
+          </div>
         </div>
         <div className={s.rigth}>
-          <AuthInput
-            value={data.login}
-            onChange={(e) => setData({ ...data, login: e.target.value })}
-            label={"Логин"}
-          />
-          <AuthInput
-            value={data.password}
-            onChange={(e) => setData({ ...data, password: e.target.value })}
-            label={"Пароль"}
-          />
-          <AuthInput
-            value={data.name}
-            onChange={(e) => setData({ ...data, name: e.target.value })}
-            label={"Имя"}
-          />
+          <div className={s.logo}>StarLine</div>
+          <h1 className={s.title}>Создай свой аккант</h1>
+          {getInputByStep()}
+
           <div className={s.btn_block}>
-            <button type="submit" className={s.btn}>
-              Зарегистрироваться
+            <button
+              style={{ marginBottom: 50 }}
+              type={"button"}
+              onClick={
+                step !== 3 ? () => setStep(step + 1) : () => props.reg(data)
+              }
+              className={s.btn}
+            >
+              {step === 3 ? "Зарегистрироваться" : "Далее"}
+            </button>
+          </div>
+          <div className={s.spacer}>
+            <span>Или</span>
+          </div>
+          <div className={s.btn_block}>
+            <button
+              style={{
+                marginBottom: 10,
+                gap: 5,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              type="button"
+              className={s.btn}
+            >
+              <img src={google} />
+              Google
+            </button>
+            <button
+              type="button"
+              className={s.btn}
+              style={{
+                gap: 5,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <img src={apple} />
+              Apple
             </button>
           </div>
           <div className={s.reg} onClick={() => props.toAuth()}>
-            Войти
+            Уже есть аккаунт? <span>Войдите в него</span>
           </div>
         </div>
       </div>
